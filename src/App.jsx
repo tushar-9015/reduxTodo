88448106816221e7a8775ae67e50e379d9e19540
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-10-09 03:03:03 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import TodoInputBox from "./components/TodoInputBox"
+import TodoItems from "./components/TodoItems"
+
+
+function App() {
+
+
+  return (
+    <div className="text-center ">
+    <h1 className="pt-5 text-4xl font-semibold">Learn about redux toolkit</h1>
+    <TodoInputBox />
+    <TodoItems />
+  </div>
+  )
+}
+
+export default App
